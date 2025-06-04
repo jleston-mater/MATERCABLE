76e0c5b5a8cdf34ef35abb8e0875090ff38ab47c
Jordan Leston <jordan@materfrance.fr>
2025-06-04 15:23:46 +0200

    Updating Layout
    Added Header component
    Added Formula component
    Added Flowbite package
    Updated Main logo
    Updated SVG Diagram

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center bg-white p-4">
+	<a href="https://www.materfrance.fr" target="_blank">
+		<img src="../logo-mater.webp" alt="Company Logo" class=" w-50 object-contain" />
+	</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import CableForm from '../components/CableForm.svelte';
 	import CableDiagram from '../components/CableDiagram.svelte';
+	import Header from '../components/Header.svelte';
 
 	import { cableTypes, Sections } from '$lib/constants';
 	import { calculateCableSectionRaw, calculateCableSectionDeducted } from '$lib/calculations';
+	import Formula from '../components/Formula.svelte';
 
 	let cableType = $state('copper');
 	let cableLength = $state(15);
@@ -25,57 +27,36 @@
 	});
 </script>
 
-<div class="mt-10 flex flex-col items-start justify-center gap-10 px-4 lg:flex-row">
-	<div class="w-full max-w-md bg-white p-6 lg:w-1/2">
-		<div class="mb-4 flex justify-center">
-			<a href="https://www.materfrance.fr" target="_blank">
-				<img src="../fleur_mater.png" alt="Company Logo" class="h-20 w-20 object-contain" />
-			</a>
-		</div>
+<div>
+	<Header />
 
-		<h2 class="mb-6 text-center text-2xl font-bold text-gray-800">
-			Calculateur de section de câble électrique
-		</h2>
+	<div class="place-items-center bg-slate-200 lg:flex-row">
+		<div class=" bg-slate-200 p-4 px-4">
+			<div
+				class="flex flex-col items-center justify-center rounded-4xl border border-indigo-900 bg-white lg:flex-row"
+			>
+				<CableForm bind:cableType bind:cableLength bind:power bind:voltageDropPercentage />
 
-		<CableForm bind:cableType bind:cableLength bind:power bind:voltageDropPercentage />
-
-		<div class="rounded-md border border-orange-200">
-			<div id="result" class=" p-4 text-center text-lg">
-				<div class=" text-lg text-gray-800">
-					<div class="flex items-center justify-center">
-						<div class="mr-2">Section de câble =</div>
-						<div class="flex flex-col items-center">
-							<div class="border-b-2 border-gray-500 pb-1">
-								<span>&rho;</span> &times; 2L &times; P
-							</div>
-							<div class="pt-1">&Delta;U &times; U</div>
-						</div>
-						<div>&nbsp≈ {cableSectionRaw}</div>
-					</div>
-				</div>
+				<CableDiagram bind:cableSectionDeducted />
 			</div>
 		</div>
-		<div
-			id="errorMessage"
-			class="mt-4 hidden rounded-md border border-red-200 bg-red-100 p-3 text-center text-red-800"
-		></div>
-
-		<p class="mt-6 text-center text-sm text-gray-600">
-			Ces calculs sont des estimations. Toujours consulter les normes électriques locales (e.g., <a
-				class="text-blue-600 visited:text-purple-600"
-				href="https://fr.wikipedia.org/wiki/NF_C_15-100"
-				target="_blank">NF C 15-100</a
-			>) et un professionnel qualifié pour des installations réelles.
-		</p>
-		<p class="mt-6 text-center text-sm text-gray-600">
-			© -
-			<a
-				class="text-blue-600 visited:text-purple-600"
-				href="https://www.materfrance.fr/"
-				target="_blank">MATERFRANCE</a
-			> - Tous droits réservés
-		</p>
+		<Formula bind:cableSectionRaw />
+		<div class="p-4">
+			<p class="mt-6 text-center text-sm text-black">
+				Ces calculs sont des estimations. Toujours consulter les normes électriques locales (e.g., <a
+					class="text-blue-600 visited:text-purple-600"
+					href="https://fr.wikipedia.org/wiki/NF_C_15-100"
+					target="_blank">NF C 15-100</a
+				>) et un professionnel qualifié pour des installations réelles.
+			</p>
+			<p class="mt-6 text-center text-sm text-black">
+				© -
+				<a
+					class="text-blue-600 visited:text-purple-600"
+					href="https://www.materfrance.fr/"
+					target="_blank">MATERFRANCE</a
+				> - Tous droits réservés
+			</p>
+		</div>
 	</div>
-
-	<CableDiagram bind:cableSectionDeducted />
 </div>
