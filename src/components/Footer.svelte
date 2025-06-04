b89c952aefacd1c4209b2787c01e4044fa5c563b
Jordan Leston <jordan@materfrance.fr>
2025-06-04 15:25:44 +0200

    Updating Layout #2
    Added Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<div class="p-4">
+	<p class="mt-6 text-center text-sm text-black">
+		Ces calculs sont des estimations. Toujours consulter les normes électriques locales (e.g., <a
+			class="text-blue-600 visited:text-purple-600"
+			href="https://fr.wikipedia.org/wiki/NF_C_15-100"
+			target="_blank">NF C 15-100</a
+		>) et un professionnel qualifié pour des installations réelles.
+	</p>
+	<p class="mt-6 text-center text-sm text-black">
+		© -
+		<a
+			class="text-blue-600 visited:text-purple-600"
+			href="https://www.materfrance.fr/"
+			target="_blank">MATERFRANCE</a
+		> - Tous droits réservés
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import { cableTypes, Sections } from '$lib/constants';
 	import { calculateCableSectionRaw, calculateCableSectionDeducted } from '$lib/calculations';
 	import Formula from '../components/Formula.svelte';
+	import Footer from '../components/Footer.svelte';
 
 	let cableType = $state('copper');
 	let cableLength = $state(15);
@@ -29,7 +30,6 @@
 
 <div>
 	<Header />
-
 	<div class="place-items-center bg-slate-200 lg:flex-row">
 		<div class=" bg-slate-200 p-4 px-4">
 			<div
@@ -41,22 +41,6 @@
 			</div>
 		</div>
 		<Formula bind:cableSectionRaw />
-		<div class="p-4">
-			<p class="mt-6 text-center text-sm text-black">
-				Ces calculs sont des estimations. Toujours consulter les normes électriques locales (e.g., <a
-					class="text-blue-600 visited:text-purple-600"
-					href="https://fr.wikipedia.org/wiki/NF_C_15-100"
-					target="_blank">NF C 15-100</a
-				>) et un professionnel qualifié pour des installations réelles.
-			</p>
-			<p class="mt-6 text-center text-sm text-black">
-				© -
-				<a
-					class="text-blue-600 visited:text-purple-600"
-					href="https://www.materfrance.fr/"
-					target="_blank">MATERFRANCE</a
-				> - Tous droits réservés
-			</p>
-		</div>
+		<Footer />
 	</div>
 </div>
